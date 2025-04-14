b191c863e8bb620d79051f3e60e3abe9bb3cd531
KabirNavadiya <kabirnavadia27@gmail.com>
2025-04-14 15:32:23 +0530

    TE-T349 PHP : Implemented Temperature measurement conversion

---
+++ classes/Temperature.php
@@ -0,0 +1,36 @@
+<?php
+
+class Temperature
+{
+    // values are in kilograms.
+    private $units = ['celsius', 'fahrenheit', 'kelvin'];
+    public function convert($value, $from, $to)
+    {
+        if (!in_array($from, $this->units) || !in_array($to, $this->units)) {
+            return "Invalid Unit";
+        }
+
+        switch ($from) {
+            case 'fahrenheit':
+                $value = ($value - 32) * 5 / 9;
+                break;
+            case 'kelvin':
+                $value = $value - 273.15;
+                break;
+        }
+
+        switch ($to) {
+            case 'fahrenheit':
+                return ($value * 9 / 5) + 32;
+            case 'kelvin':
+                return $value + 273.15;
+            default:
+                return $value;
+        }
+    }
+
+    public function getUnits()
+    {
+        return $this->units;
+    }
+}
--- temperature.php
+++ temperature.php
@@ -1,11 +1,11 @@
 <?php
 require_once 'request_handler.php';
-require_once 'classes/MassAndWeight.php';
-$converter = new MassAndWeight();
+require_once 'classes/Temperature.php';
+$converter = new Temperature();
 $data = handleRequest($converter);
 $units = $converter->getUnits();
 
-echo '<h1>Mass & Weight Converter</h1>';
+echo '<h1>Temperature Converter</h1>';
 include 'form.php';
 
 
