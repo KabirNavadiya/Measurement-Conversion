f3b2dc08de92e8a3be642f225fe36e0ea051e5ca
KabirNavadiya <kabirnavadia27@gmail.com>
2025-04-14 15:11:40 +0530

    TE-T349 PHP : Implemented Mass and weight measurement conversion

---
+++ classes/MassAndWeight.php
@@ -0,0 +1,29 @@
+<?php
+
+class MassAndWeight
+{
+    // values are in kilograms.
+    private $units = [
+        'kilogram' => 1,
+        'gram' => 0.001,
+        'tonne' => 1000,
+        'milligram' => 0.000001,
+        'microgram' => 0.000000001,
+        'pound' => 0.453592
+    ];
+
+    public function convert($value, $from, $to)
+    {
+        if (!isset($this->units[$from]) || !isset($this->units[$to])) {
+            return "Invalid Unit";
+        }
+
+        $ans = $value * $this->units[$from];
+        return $ans / $this->units[$to];
+    }
+
+    public function getUnits()
+    {
+        return array_keys($this->units);
+    }
+}
