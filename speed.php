1f0260e447d37d0ea43fb1ca6110c21665de8590
KabirNavadiya <kabirnavadia27@gmail.com>
2025-04-14 15:18:43 +0530

    TE-T349 PHP : Implemented Speed measurement conversion

---
+++ speed.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'request_handler.php';
+require_once 'classes/Speed.php';
+$converter = new Speed();
+$data = handleRequest($converter);
+$units = $converter->getUnits();
+
+echo '<h1>Speed Converter</h1>';
+include 'form.php';
+
+
